ea852448f62fce351b815e601f556525158a5723
Mick McGrath <mick.mc.13@gmail.com>
2018-06-11 00:15:06 -0700

    debugging iterations

--- actions/create_file.py
+++ actions/create_file.py
@@ -1,6 +1,5 @@
 from lib.base import BaseGithubAction
 from github import GithubObject
-from lib.formatters import file_to_dict
 
 __all__ = [
     'CreateFileAction'
@@ -9,13 +8,12 @@ __all__ = [
 
 class CreateFileAction(BaseGithubAction):
     def run(self, user, repo, path, message, content,
-                    branch=github.GithubObject.NotSet,
-                    committer=github.GithubObject.NotSet,
-                    author=github.GithubObject.NotSet):
+                    branch=GithubObject.NotSet,
+                    committer=GithubObject.NotSet,
+                    author=GithubObject.NotSet):
 
-        user = self._client.get_user(user)
-        repo = user.get_repo(repo)
-        file = repo.create_file(path, message, content, branch, committer, author)
-                    
-        result = file.raw_data
-        return result
+      user = self._client.get_user(user)
+      repo = user.get_repo(repo)
+      file = repo.create_file(path, message, content, branch, committer, author)
+
+      return file
